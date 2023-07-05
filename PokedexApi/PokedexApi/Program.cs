f00895586868235c3afba3668abdbea0f775c248
cihan <cihan@pars-connect.com>
2023-07-05 16:50:44 +0300

    Add swagger

--- PokedexApi/PokedexApi/Program.cs
+++ PokedexApi/PokedexApi/Program.cs
@@ -1,15 +1,29 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pokedex", Version = "v1" });
+});
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pokedex v1");
 
+});
 app.UseAuthorization();
 
 app.MapControllers();
