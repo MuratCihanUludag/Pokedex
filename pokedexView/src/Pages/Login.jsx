08c9da548c6ab4a1b8c0b3331f41e3407c17b600
cihan <cihan@pars-connect.com>
2023-07-07 17:56:02 +0300

    start login page

--- pokedexView/src/Pages/Login.jsx
+++ pokedexView/src/Pages/Login.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import Pokeball from "../assets/pokeball.png";
 function Login() {
   return (
     <div>
-      Login
+      <div className="login-backgorund">
+        <div className="left-half"></div>
+        <div className="right-half"></div>
+        <div className="icon-position">
+          <img className="pokeball-icon" src={Pokeball} alt="" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
